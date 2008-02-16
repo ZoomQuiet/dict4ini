8ef9286ea65ffcb6739b70cabcea48f9706d80b2
limodou <limodou@c2a10005-5334-0410-ab8b-bbab12a37475>
2008-02-16 02:55:54 +0000

    0.9.4.1 released

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name="dict4ini",
-    version="0.9.4",
+    version="0.9.4.1",
     author="limodou",
     author_email="limodou@gmail.com",
     url="http://code.google.com/p/dict4ini/",
